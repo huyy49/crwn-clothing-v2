3f194b967d141f4c8c776f50b9060e4a7491837d
Yangyin Hu <huyy49@gmail.com>
2022-07-08 09:07:31 -0400

    Update firebase.utils.ts
    Update firebaseConfig.

--- src/utils/firebase/firebase.utils.ts
+++ src/utils/firebase/firebase.utils.ts
@@ -26,12 +26,12 @@ import {
 import { Category } from '../../store/categories/category.types';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyDGgCHuv01awz6We5psXqGUSEJtnoH87Fs",
+  authDomain: "crwn-clothing-db-9807a.firebaseapp.com",
+  projectId: "crwn-clothing-db-9807a",
+  storageBucket: "crwn-clothing-db-9807a.appspot.com",
+  messagingSenderId: "996789189941",
+  appId: "1:996789189941:web:014db7a89670f92608a07c"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
